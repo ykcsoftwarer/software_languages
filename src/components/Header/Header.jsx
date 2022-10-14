88cb95a1b77e0ebf56540748e6f9c4cddc01e700
ykcsoftwarer <yusufkucukcopur@gmail.com>
2022-10-14 22:09:35 +0300

    project gived tecnical error

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,6 @@ import reactLogo from '../../assets/react.svg';
 import './Header.css';
 
 const Header = () => {
-  const{name, img, options} = Card
   return (
     <div className="logo-container">
       <img className="react-logo" src={reactLogo} alt="react-logo" />
